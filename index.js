108184d54d237d203eeebb8c5a5deebed397533b
Savage Blood <malifarge.thomas9@gmail.com>
2023-01-04 13:57:35 +0100

    get toilettes

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const PORT = 5000
+const toilettesRoutes = require('./Routes/toilettes')
 
 require('./models')
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/toilettes',toilettesRoutes)
 
 app.listen(PORT,()=>{
     console.log(`Serveur running on ${PORT}`);
